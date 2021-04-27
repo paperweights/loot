522b803454077d11e8aef1f8504971869bc55334
Justinas Grigas <jstn_as@protonmail.com>
2021-04-28 00:57:43 +0100

    Added light flicker

--- actors/lighting/light_source/light_source.gd
+++ actors/lighting/light_source/light_source.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 export var _length = 32
 
+var _flicker = 0
 
 onready var _light_mesh = $LightMesh
 onready var _shade_masks = $ShadeMasks
@@ -9,6 +10,11 @@ onready var _camera: Camera2D = get_tree().get_nodes_in_group("camera")[0]
 
 
 func _process(delta) -> void:
-	_light_mesh.update_mesh(_length)
-	_shade_masks.update_masks(_length)
+	_light_mesh.update_mesh(_length - _flicker)
+	_shade_masks.update_masks(_length - _flicker)
 	return
+
+
+func _on_Timer_timeout():
+	_flicker = rand_range(0, 1)
+	pass
