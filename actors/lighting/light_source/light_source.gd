67611a8dcd02bf8ee042bafdfeb4e41e7aa603d3
Justinas Grigas <jstn_as@protonmail.com>
2021-04-28 23:21:18 +0100

    Light flicker now smooth
    - Added steam lords palette.

--- actors/lighting/light_source/light_source.gd
+++ actors/lighting/light_source/light_source.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
-export var _length = 32
+export var _target_length = 32
+export (float, 0, 1) var _lerp_speed = 0.02
 
+var _target_flicker = 0
 var _flicker = 0
+var _length = 0
 
 onready var _light_mesh = $LightMesh
 onready var _shade_masks = $ShadeMasks
@@ -10,11 +13,13 @@ onready var _camera: Camera2D = get_tree().get_nodes_in_group("camera")[0]
 
 
 func _process(delta) -> void:
+	_length = lerp(_length, _target_length, _lerp_speed)
+	_flicker = lerp(_flicker, _target_flicker, 0.02)
 	_light_mesh.update_mesh(_length - _flicker)
 	_shade_masks.update_masks(_length - _flicker)
 	return
 
 
 func _on_Timer_timeout():
-	_flicker = rand_range(0, 1)
+	_target_flicker = rand_range(-4, 4)
 	pass
