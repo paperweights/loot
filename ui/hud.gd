3fad2f27be45dde87c1182dfd00e24710b5c866d
Justinas Grigas <jstn_as@protonmail.com>
2021-05-16 14:22:48 +0100

    Added menu tab switching.
    Switching between menu tabs can now be done using keys.
    
    Tab - toggles visibility
    E - Equip
    B - Bag
    M - Map
    
    - Added label theme.

--- ui/hud.gd
+++ ui/hud.gd
@@ -1,8 +1,27 @@
-extends Control
+extends TabContainer
 
+const TOGGLE_ACTION = "menu_toggle"
+const MENU_ACTIONS = ["menu_equip", "menu_bag", "menu_map"]
 
 func _unhandled_input(event: InputEvent) -> void:
-	if not event.is_action_pressed("ui_focus_next"):
+	# Toggle
+	if event.is_action_pressed(TOGGLE_ACTION):
+		_switch_tab(current_tab)
 		return
-	visible = !visible
+	# Tabs.
+	for t in range(len(MENU_ACTIONS)):
+		if event.is_action_pressed(MENU_ACTIONS[t]):
+			_switch_tab(t)
+			return
+	return
+
+
+func _switch_tab(new_tab: int) -> void:
+	# Switch to new tab if current one is not the same.
+	if current_tab != new_tab:
+		current_tab = new_tab
+		visible = true
+	# Toggle visibility otherwise.
+	else:
+		visible = not visible
 	return
