31d8fc8947d83332e63127032e47eee96dbc6c3b
Justinas Grigas <jstn_as@protonmail.com>
2021-05-16 17:05:41 +0100

    Fixed Z Indexer
    The Z index is now offset by the position of the camera, so it will
    never max out (go above 4096)
    Might need to be clamped between -128 and 128.

--- actors/z_indexer.gd
+++ actors/z_indexer.gd
@@ -3,9 +3,14 @@ extends Node2D
 
 export (NodePath) var _target_node = ""
 
+onready var _camera: Camera2D = get_tree().get_nodes_in_group("camera")[0]
 onready var _target: Node2D = get_node(_target_node)
 
 
 func _process(delta) -> void:
-	_target.z_index = _target.global_position.y
+	_target.z_index = _get_z_index()
 	return
+
+
+func _get_z_index() -> int:
+	return int(_target.global_position.y - _camera.global_position.y + 64)
