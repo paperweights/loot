125948adec9bc771a1efb19afa9baefc59271aa0
Justinas Grigas <jstn_as@protonmail.com>
2021-05-17 02:44:03 +0100

    Updated heart display
    Heart display now has an update hearts function which updates the
    textures of the hearts without having to recreate them. It still needs
    to be connected to health signals.

--- nodes/heart_display.gd
+++ nodes/heart_display.gd
@@ -4,6 +4,9 @@ extends GridContainer
 const HEART_FULL = preload("res://ui/sprites/hearts/full.png")
 const HEART_HALF = preload("res://ui/sprites/hearts/half.png")
 const HEART_EMPTY = preload("res://ui/sprites/hearts/empty.png")
+const HEART_TEXTURE_RECT = preload("res://ui/heart_texture_rect.tscn")
+
+var _heart_texture_rects = []
 
 onready var _player_health: Health = get_tree().get_nodes_in_group("player_health")[0]
 
@@ -16,20 +19,30 @@ func _init_hearts() -> void:
 	# Destroy current hearts.
 	for child in get_children():
 		child.queue_free()
+	_heart_texture_rects = []
 	# Figure out how many hearts will be needed.
 	var heart_count = ceil(_player_health.get_max_health() / 2.0)
 	# Create new hearts.
-	var health = _player_health.get_health()
-	print(health)
 	for h in range(heart_count):
-		var heart_health = h * 2 + 2
-		print(heart_health)
-		var new_heart = TextureRect.new()
-		# Choose the texture based on current health.
-		if health >= heart_health:
-			new_heart.texture = HEART_FULL
-		elif heart_health - 1 == health:
-			new_heart.texture = HEART_HALF
+		var new_heart_texture_rect = HEART_TEXTURE_RECT.instance()
+		add_child(new_heart_texture_rect)
+		_heart_texture_rects.append(new_heart_texture_rect)
+	_update_hearts(_player_health.get_health())
+
+
+func _update_hearts(health: int) -> void:
+	var health_threshold = 2
+	for heart_texture_rect in _heart_texture_rects:
+		if health >= health_threshold:
+			heart_texture_rect.texture = HEART_FULL
+		elif health == health_threshold - 1:
+			heart_texture_rect.texture = HEART_HALF
 		else:
-			new_heart.texture = HEART_EMPTY
-		add_child(new_heart)
+			heart_texture_rect.texture = HEART_EMPTY
+		health_threshold += 2
+	return
+
+
+func _on_PlayerHealth_health_changed(new_health: int) -> void:
+	_init_hearts()
+	return
