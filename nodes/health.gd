28d3cc67310d28874c2ccdfe5c7e682f76527a0e
Justinas Grigas <jstn_as@protonmail.com>
2021-11-14 09:54:37 +0000

    updated heart display

--- nodes/health.gd
+++ nodes/health.gd
@@ -1,11 +1,20 @@
 class_name Health
 extends Node
 
+signal health_changed(health)
+signal max_health_changed(max_health)
 signal died
 
-export(int) var _max_health = 8
+export(int) var _max_health = 6
 
 onready var _health = _max_health
+
+
+func _ready():
+	# Update UI
+	get_tree().call_group("hearts", "max_health_changed", _max_health)
+	get_tree().call_group("hearts", "health_changed", _health)
+	hurt(3)
 
 
 func get_max_health() -> int:
@@ -16,22 +25,22 @@ func get_health() -> int:
 	return _health
 
 
-func heal(amount: int) -> void:
-	# Amount can't be below 0.
+func heal(amount: int):
+	# Amount can't be below 0
 	_change_health(max(0, amount))
-	return
 
 
-func hurt(amount: int) -> void:
-	# Amount can't be above 0.
-	_change_health(min(0, amount))
-	return
+func hurt(amount: int):
+	# Amount can't be above 0
+	_change_health(min(0, -amount))
 
 
-func _change_health(amount: int) -> void:
+func _change_health(amount: int):
 	_health += amount
 	_health = clamp(_health, 0, _max_health)
 	# Die if health is too low.
 	if _health == 0:
 		emit_signal("died")
-	return
+	else:
+		emit_signal("health_changed", _health)
+		get_tree().call_group("hearts", "health_changed", _health)
---
+++ ui/hearts/hearts.gd
@@ -0,0 +1,45 @@
+class_name Hearts
+extends GridContainer
+
+const HEARTS = {
+	"Full": preload("res://art/hearts/full.png"),
+	"Half": preload("res://art/hearts/half.png"),
+	"Empty": preload("res://art/hearts/empty.png"),
+}
+const HEART_RECT = preload("res://ui/hearts/heart_rect.tscn")
+
+var _heart_rects = []
+
+
+func _init_hearts(max_health: int):
+	# Destroy current hearts.
+	for child in get_children():
+		child.queue_free()
+	_heart_rects = []
+	# Figure out how many hearts will be needed.
+	var heart_count = ceil(max_health / 2.0)
+	# Create new hearts.
+	for h in range(heart_count):
+		var new_heart_texture_rect = HEART_RECT.instance()
+		add_child(new_heart_texture_rect)
+		_heart_rects.append(new_heart_texture_rect)
+
+
+func _update_hearts(health: int):
+	var health_threshold = 2
+	for heart_texture_rect in _heart_rects:
+		if health >= health_threshold:
+			heart_texture_rect.texture = HEARTS["Full"]
+		elif health == health_threshold - 1:
+			heart_texture_rect.texture = HEARTS["Half"]
+		else:
+			heart_texture_rect.texture = HEARTS["Empty"]
+		health_threshold += 2
+
+
+func max_health_changed(max_health: int):
+	_init_hearts(max_health)
+
+
+func health_changed(health: int):
+	_update_hearts(health)
