aa08e15e64d7dfd83631cf3caa1aba5a67ad821d
Justinas Grigas <jstn_as@protonmail.com>
2021-12-06 15:33:45 +0000

    menus are now toggled properly

--- ui/hud.gd
+++ ui/hud.gd
@@ -1,27 +1,38 @@
 extends TabContainer
 
+signal toggled(new_state)
+
 const TOGGLE_ACTION = "menu_toggle"
 const MENU_ACTIONS = ["menu_equip", "menu_bag", "menu_map"]
+const MENU_COUNT = len(MENU_ACTIONS)
 
-func _unhandled_input(event: InputEvent) -> void:
+
+func _unhandled_input(event: InputEvent):
 	# Toggle
 	if event.is_action_pressed(TOGGLE_ACTION):
 		_switch_tab(current_tab)
 		return
 	# Tabs.
-	for t in range(len(MENU_ACTIONS)):
+	for t in range(MENU_COUNT):
 		if event.is_action_pressed(MENU_ACTIONS[t]):
 			_switch_tab(t)
 			return
 	return
 
 
-func _switch_tab(new_tab: int) -> void:
+func _switch_tab(new_tab: int):
 	# Switch to new tab if current one is not the same.
 	if current_tab != new_tab:
 		current_tab = new_tab
 		visible = true
 	# Toggle visibility otherwise.
 	else:
-		visible = not visible
+		visible = !visible
+	emit_signal("toggled", visible)
 	return
+
+
+# Disable hud when menu turns on
+func _on_MainMenu_toggled(new_state):
+	if visible and new_state:
+		_switch_tab(current_tab)
--- ui/menu/menu.gd
+++ ui/menu/menu.gd
@@ -1,6 +1,24 @@
 extends PanelContainer
 
+signal toggled(new_state)
+
+export (bool) var _can_toggle = true
+
 
 func _input(event):
-	if event is InputEventKey and event.scancode == KEY_ESCAPE and event.pressed:
-		visible = !visible
+	if event is InputEventKey and event.scancode == KEY_ESCAPE \
+	and event.pressed:
+		set_state(!visible)
+
+
+func set_state(enabled: bool):
+	if !_can_toggle:
+		return
+	visible = enabled
+	emit_signal("toggled", enabled)
+
+
+# Disable menu when hud turns on
+func _on_Menu_toggled(new_state):
+	if visible and new_state:
+		set_state(false)
