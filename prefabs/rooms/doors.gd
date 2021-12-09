38f65fa487f3e3f5fd80d2698fb268e2db4d6c54
Justinas Grigas <jstn_as@protonmail.com>
2021-12-09 18:08:10 +0000

    changed doors resource to room script
    the doors that a room supports are now specified in the room script

--- prefabs/rooms/doors.gd
+++
@@ -1,5 +0,0 @@
-extends Resource
-class_name Doors
-
-export(Resource) var scene
-export(int, FLAGS, "Top", "Down", "Left", "Right") var doors = 15
---
+++ prefabs/rooms/room.gd
@@ -0,0 +1,3 @@
+extends Node2D
+
+export(int, FLAGS, "Top", "Down", "Left", "Right") var _doors = 15
