ac6560b14eefbf53b63580cb47f8bb4ea9672ce0
Justinas Grigas <jstn_as@protonmail.com>
2022-08-20 00:20:30 +0300

    working on dungeon generation

--- rooms/room.gd
+++ rooms/room.gd
@@ -1,17 +1,5 @@
-class_name DungeonRoom
-extends Node2D
+extends Reference
 
-"""
-conncections order: UP, LEFT, DOWN, RIGHT
-connections:
-	-1: Nothing, when connecting between cells in a room
-	0: Solid wall
-	1: Exit
-	2: Empty corridor
-	3: Door
-"""
-export(Dictionary) var cells = {
-	Vector2(): {
-		'connections': PoolIntArray([2, 2, 2, 2])
-	}
-}
+
+var scene: PackedScene
+var cells: Dictionary
--- rooms/rooms.gd
+++ rooms/rooms.gd
@@ -1,22 +1,63 @@
+"""
+Room Generator
+"""
 extends Node2D
+
+enum Connection {
+	Nothing = -1, # no wall or connection
+	Solid, # solid wall
+	Exit, # dungeon exit
+	Pass, # passage
+}
 
 const CELL_SIZE = Vector2(8, 8)
 const ROOM_SIZE = Vector2(16, 16)
 const TILEMAPS = ["Floors", "Walls", "Tops"]
 # borders
 const BORDERS = [
-	[Vector2(), preload("res://rooms/borders/horizontal.tscn")],
-	[Vector2(), preload("res://rooms/borders/vertical.tscn")],
-	[Vector2(0, 16), preload("res://rooms/borders/horizontal.tscn")],
-	[Vector2(16, 0), preload("res://rooms/borders/vertical.tscn")],
+	{
+		'scene': preload("res://rooms/borders/horizontal.tscn"),
+		'offset': Vector2(),
+		'connections': {
+			'solid': preload("res://rooms/borders/solid.tscn"),
+			'passage': preload("res://rooms/borders/passage.tscn"),
+		}
+	},
+	{
+		'scene': preload("res://rooms/borders/vertical.tscn"),
+		'offset': Vector2(),
+	},
+	{
+		'scene': preload("res://rooms/borders/horizontal.tscn"),
+		'offset': Vector2(0, 16),
+	},
+	{
+		'scene': preload("res://rooms/borders/vertical.tscn"),
+		'offset': Vector2(16,0),
+	},
 ]
 const OFFSETS = [Vector2.UP, Vector2.LEFT, Vector2.DOWN, Vector2.RIGHT]
+const STARTING_ROOM = {
+	'scene': preload("res://rooms/prefabs/starting_room.tscn"),
+	'cells': {
+		Vector2(): [Connection.Pass, Connection.Pass, Connection.Pass,
+		Connection.Pass],
+	}
+}
+const PALETTE = [
+	{
+		'scene': preload("res://rooms/prefabs/template.tscn"),
+		'cells': {
+			Vector2(): [Connection.Pass, Connection.Pass, Connection.Pass,
+			Connection.Pass],
+		},
+	},
+]
 
-export(PackedScene) var _starting_room
 export(Array, Dictionary) var _veins = [
 	{
 		'length': 3,
-		'palette': load("res://rooms/palettes/palette.tres")
+		'palette': PALETTE,
 	},
 ]
 
@@ -24,37 +65,41 @@ onready var _tilemaps = [$Floor, $Walls, $Tops]
 
 
 func _ready():
-	_build()
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var rooms = _generate_layout(rng)
+	_place_rooms(rooms)
+	_update_tilemaps()
 	return
 
 
-func _build(seed_: String = "") -> void:
-	var rng = RandomNumberGenerator.new()
-	if seed_:
-		rng.set_seed(hash(seed_))
-	else:
-		rng.randomize()
-		print('yeah')
-	# keep track of available and occupied dungeon cells
-	var occupied = PoolVector2Array([Vector2()])
-	var available: Dictionary = {}
-	# spawn starting room
-	_add_available(_spawn_room(_starting_room, Vector2()), available)
-	# build veins
+func _generate_layout(rng: RandomNumberGenerator) -> Dictionary:
+	"""
+	generate the dungeon layout by keeping track of the rooms to spawn and the
+	location to spawn them at. This will be followed up by a seperate function
+	that just draws the rooms and the connections to the tilemaps.
+	"""
+	var rooms = {}
+	# spawn the starting room
+	rooms[Vector2()] = STARTING_ROOM
+	# build each vein
 	for vein in _veins:
-		var palette: RoomPalette = vein['palette']
-		var room_count = palette.get_length()
-		for l in range(vein['length']):
-			# pick the room to spawn
-			var room = palette.get_room(rng.randi() % room_count)
-			# pick the location
-			var locations = available.keys()
-			var location = locations[rng.randi() % len(locations)]
-			occupied.push_back(location)
-			_add_available(_spawn_room(room, location), available)
-	# update bitmasks once all tilemap changes have been made
-	for tilemap in _tilemaps:
-		tilemap.update_bitmask_region()
+		var palette: Array = vein['palette']
+		# build a specified number of rooms
+		for r in range(vein['length']):
+			var room_order = _shuffle(range(len(palette)), rng)
+			# pick a room prefab to build
+			for p in room_order:
+				var room = palette[p]
+				# figure out where to place the room
+	return rooms
+
+
+func _place_rooms(rooms: Dictionary) -> void:
+	for location in rooms.keys():
+		var room = rooms[location]
+		_spawn_room(room['scene'], location)
+		_spawn_borders(room['cells'], location)
 	return
 
 
@@ -64,31 +109,34 @@ func _add_available(from: Dictionary, to: Dictionary) -> void:
 	return
 
 
-func _spawn_room(room: PackedScene, location: Vector2) -> Dictionary:
-	var instance: DungeonRoom = room.instance()
-	# Combine tilemaps
+func _spawn_room(room: PackedScene, location: Vector2) -> void:
 	var offset = location * ROOM_SIZE
-	_blit_tilemaps(instance, offset)
-	# Generate borders
-	var available: Dictionary = {}
-	for cell in instance.cells:
-		var data: Dictionary = instance.cells[cell]
+	_blit_tilemaps(room.instance(), offset)
+	return
+
+
+func _spawn_borders(cells: Dictionary, location: Vector2) -> void:
+	var offset = location * ROOM_SIZE
+	for cell in cells.keys():
+		var connections = cells[cell]
 		for c in range(4):
-			var type: int = data['connections'][c]
-			# negative types make their own walls
-			if type < 0:
+			var connection = connections[c]
+			if connection == Connection.Nothing:
 				continue
 			var border = BORDERS[c]
-			_blit_tilemaps(border[1].instance(), border[0] + offset)
-			# 0 and 1 don't connect
-			if type < 2:
+			var border_offset = offset + border['offset']
+			_blit_tilemaps(border['scene'].instance(), border_offset)
+			if c > 0:
 				continue
-			var available_location: Vector2 = location + cell + OFFSETS[c]
-			if available_location in available:
-				available[available_location] += 1 << c
-			else:
-				available[available_location] = 1 << c
-	return available
+			var passage: PackedScene = null
+			match connection:
+				Connection.Solid:
+					passage = border['connections']['solid']
+				Connection.Pass:
+					passage = border['connections']['passage']
+			if passage:
+				_blit_tilemaps(passage.instance(), border_offset)
+	return
 
 
 func _blit_tilemap(src: TileMap, dst: TileMap, offset: Vector2) -> void:
@@ -103,3 +151,23 @@ func _blit_tilemaps(room: Node2D, offset: Vector2) -> void:
 	for t in range(3):
 		_blit_tilemap(room.get_node(TILEMAPS[t]), _tilemaps[t], offset)
 	return
+
+
+func _update_tilemaps() -> void:
+	for tilemap in _tilemaps:
+		tilemap.update_bitmask_region()
+	return
+
+
+func _shuffle(array: Array, rng: RandomNumberGenerator) -> Array:
+	"""
+	Fisher-Yates shuffle
+	https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#Modern_method
+	"""
+	var copy = array.duplicate()
+	for i in range(len(array) - 1, 0, -1):
+		var j = rng.randi_range(0, i)
+		var value = copy[i]
+		copy[i] = copy[j]
+		copy[j] = value
+	return copy
